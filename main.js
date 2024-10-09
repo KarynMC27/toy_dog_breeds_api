c73ba593aa2dce84a6f7cd6fb01296f0a2246446
KarynMC27 <karyncasper@gmail.com>
2024-10-09 13:01:10 -0700

    second commit

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const breedName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://xxxx${breedName}`)
+        const response = await fetch(`https://toy-dog-breeds-api-1.onrender.com/api/${breedName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.name
+        document.querySelector('h2').innerText = `${data.name} ${data.height} ${data.weight} ${data.origin_regions} ${data.reputation}`
     }catch(error){
         console.log(error)
     }
